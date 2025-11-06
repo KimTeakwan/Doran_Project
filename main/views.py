6b0c4467e355951552977c556c501fe3aed7ce88
son <sjss1936@naver.com>
2025-11-06 14:37:09 +0900

    profile

--- main/views.py
+++ main/views.py
@@ -33,7 +33,36 @@ def create(request):
     return render(request, 'main/create.html')
 
 def profile(request):
-    return render(request, 'main/profile.html')
+    user_profile = {
+        'name': 'sikboyyy',
+        'username': '@sikboyyy',
+        'bio': 'This is my Doran project. I love coding and building awesome things.',
+        'banner_url': '/static/main/img/bb.jpg',
+        'profile_picture_url': '/static/main/img/user1.png',
+        'following': 123,
+        'followers': 456,
+    }
+    posts = [
+        {
+            'username': 'sikboyyy',
+            'profile_picture': '/static/main/img/user1.png',
+            'image_url': '/static/main/img/aa.jpg',
+            'likes': 1234,
+            'caption': '나 좀 잘생긴 거 같아.'
+        },
+        {
+            'username': 'sikboyyy',
+            'profile_picture': '/static/main/img/user1.png',
+            'image_url': '/static/main/img/bb.jpg',
+            'likes': 567,
+            'caption': '이건 좀 귀여운듯.'
+        }
+    ]
+    context = {
+        'user': user_profile,
+        'posts': posts
+    }
+    return render(request, 'main/profile.html', context)
 
 def login_view(request):
     return render(request, 'main/login.html')
